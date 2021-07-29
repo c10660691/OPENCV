116e774335cb67416ec368c9bebe755758d45df9
c10660691 <c10660691@gmail.com>
2021-07-29 10:22:10 +0800

    hist練習

--- test22.py
+++ test22.py
@@ -4,10 +4,6 @@ from matplotlib import pyplot as plt
 img = cv.imread('001.jpg')
 img1 = cv.imread('001.jpg', 0)
 color = ('b', 'g', 'r')
-# for i,col in enumerate(color):
-#     histr = cv.calcHist([img],[i],None,[256],[0,256])
-#     plt.plot(histr,color = col)
-#     plt.xlim([0,256])
 histr = cv.calcHist([img], [0], None, [256], [0, 256])
 plt.plot(histr)
 plt.xlim([0, 256])
